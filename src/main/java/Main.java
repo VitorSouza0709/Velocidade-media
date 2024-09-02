dc8058e402d87a4dd3d2c03c2d5eb345e3f3a69a
Vitor <169068391+VitorSouza0709@users.noreply.github.com>
2024-09-02 12:17:04 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Programa pra calcular a velocidade km/h");
+    System.out.println();
+    System.out.println("A distância considerada é:");
+    double distancia = 400;
+    System.out.println(distancia);
+    System.out.println("O tempo gasto é:");
+    double tempo = 3;
+    System.out.println(tempo);
+    double velocidadeMedia = distancia / tempo;
+    System.out.println("A velocidade média é:");
+    System.out.println(velocidadeMedia);
+    
+  }
+
+  
+}
